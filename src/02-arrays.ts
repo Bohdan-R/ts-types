0970bfab307ffae370b9b47e54d517d8112a66b9
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-08-07 10:28:20 +0300

    arrays

--- src/02-arrays.ts
+++ src/02-arrays.ts
@@ -6,4 +6,18 @@
  *  - Tuple `[тип, тип]`, например массив координат
  */
 
+const temps: readonly number[] = [12, 45, 35, 45, 14, 47,]
+
+/* temps.push(5) */
+/* const temps: Array<number> = [12, 45, 35, 45, 14, 47] */
+/* const temps: (number | string)[] = [12, 45, 35, 45, 14, 47, 'sdfsdf'] */
+
+
+const coords: [number, number] = [45.1244, 475.4545]
+const rgb: [number,number,number] = [255,45,100]
+
+const arrA: number[] = [1, 2, 3, 4, 5]
+const arrB = [...arrA]
+
+console.log(temps,coords, rgb);
 export {};
