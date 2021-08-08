e14726093cd5dd1a645b5d7f2871d0c8edb27401
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-08-08 09:45:18 +0300

    types

--- src/03-types.ts
+++ src/03-types.ts
@@ -4,4 +4,18 @@
  * - Состояние с union
  */
 
+type ID = number | string
+const userId: ID = 555
+const postId: ID = 'sdfsdf455df';
+
+type Coords = [number, number]
+const coords: Coords = [50.456, 45.4566]
+
+type ReqStatus = 'request' | 'success' | 'error'
+const requestStatus: ReqStatus = 'request'
+
+type CellSize = 2 | 4 | 8
+const cell: CellSize = 4
+
+console.log(userId, postId, coords, requestStatus);
 export {};
