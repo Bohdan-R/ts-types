acb775f43512c679ccaba9981314beebeeeff510
Bohdan Remeniuk <bogdanremenjuk@gmail.com>
2021-08-09 13:39:45 +0300

    classes

--- src/07-classes.ts
+++ src/07-classes.ts
@@ -9,4 +9,43 @@
  *  - Интерфейс класса с implements
  */
 
+interface IParams {
+    size: string;
+    toppings: string[];
+}
+
+interface IPizza {
+    size: string;
+    addTopings(topping: string): void;
+}
+
+class Pizza implements IPizza{
+    public size: string;
+    private toppings: string[];
+
+    constructor({size, toppings = []}: IParams) {
+        this.size = size;
+        this.toppings = toppings;
+    }
+
+    private validateTopping(topping: string): boolean {
+        console.log(topping);
+        // валидация
+        return true;
+    }
+
+    public addTopings(topping: string) {
+        this.toppings.push(topping)
+    }
+}
+
+const pizza: IPizza = new Pizza({size: 'medium', toppings: ['cheese']});
+
+console.log(pizza);
+
+/* const x = pizza.addTopings('qewqwe');
+
+console.log(x); */
+
+
 export {};
